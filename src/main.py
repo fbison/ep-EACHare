52e700cda7397b3986c3da46713177755f15dca6
fbison <fbisons107@outlook.com>
2025-03-26 08:18:44 -0300

    Menu-Working

--- src/main.py
+++ src/main.py
@@ -2,18 +2,69 @@ import sys
 from collections import OrderedDict
 
 # Exit constant
-EXIT_COMMAND = '9'
+EXIT_COMMAND = 9
 
 def list_peers():
+    print("")
     #TODO: Implementation
 
-commands_functions = OrderedDict([ # OrderedDict to keep the order of insertion
-    ("Listar peers", list_peers)
-    #TODO: Add the other commands
-])
+def get_peers():
+    print("")
+    #TODO: Implementation
+
+def list_local_files():
+    print("")
+    #TODO: Implementation
+
+def search_files():
+    print("")
+    #TODO: Implementation
+
+def show_statistics():
+    print("")
+    #TODO: Implementation
+
+def change_chunk_size():
+    print("")
+    #TODO: Implementation
+
+def exit():
+    print("")
+    #TODO: Implementation
+
+commands_functions = {
+    1: {"description": "Listar peers", "function": list_peers},
+    2: {"description": "Obter peers", "function": get_peers},
+    3: {"description": "Listar arquivos locais", "function": list_local_files},
+    4: {"description": "Buscar arquivos", "function": search_files},
+    5: {"description": "Exibir estatisticas", "function": show_statistics},
+    6: {"description": "Alterar tamanho de chunk", "function": change_chunk_size},
+    EXIT_COMMAND: {"description": "Sair", "function": exit}
+}
+
 
 def show_commands():
     print("Escolha um comando:")
+    for key, value in commands_functions.items():
+        print(f"\t[{key}] {value['description']}")
+
+def execute_command(command_number):
+    if command_number not in commands_functions:
+        raise ValueError()
+    commands_functions[command_number]["function"]()
+
+def menu():
+    while True:
+        show_commands()
+        try:
+            command_number = int(input(">"))
+            if command_number == int(EXIT_COMMAND):
+                commands_functions[command_number]["function"]()
+                break
+            execute_command(command_number)
+        except ValueError:
+            print("Por favor, insira um número válido.")
+
 
 
 def main():
@@ -21,7 +72,7 @@ def main():
     peers_file = sys.argv[2]
     shared_dir = sys.argv[3]
 
-    show_commands()
+    menu()
 
 if __name__ == '__main__':
     main()
