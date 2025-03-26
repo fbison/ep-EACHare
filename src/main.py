9da75b13054a787251f72ba75eb93ac89440ecd6
Luan Pereira Pinheiro <luanpereira0603@gmail.com>
2025-03-25 21:09:26 -0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import sys
+
+def show_commands():
+    print("Escolha um comando:")
+
+address, port = sys.argv[1].split(':')
+peers_file = sys.argv[2]
+shared_dir = sys.argv[3]
+
+show_commands()
