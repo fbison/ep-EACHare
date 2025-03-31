c18b1829128005958608cd5f8edd39ca620ce8fe
fbison <fbisons107@outlook.com>
2025-03-31 20:47:32 -0300

    feat: implementation list local files

--- src/main.py
+++ src/main.py
@@ -4,11 +4,12 @@ from collections import OrderedDict
 from connection import Connection
 from peer_manager import PeerManager
 from peer import Peer
+import os
 # Exit constant
 EXIT_COMMAND = 9
 
 peer_manager = PeerManager()
-
+_shared_dir= ""
 
 def hello(peer: Peer):
     connection.send_message(peer, "HELLO")
@@ -18,8 +19,17 @@ def get_peers():
         connection.send_message(peer, "GET_PEERS")
 
 def list_local_files():
-    print("")
-    #TODO: Implementation
+    if not _shared_dir:
+        print("Erro: O diretório compartilhado não foi configurado.")
+        return
+
+    try:
+        files = os.listdir(_shared_dir)
+        if files:
+            for file in files:
+                print(f"\t- {file}")
+    except Exception as e:
+        print(f"Erro ao listar arquivos: {e}")
 
 def search_files():
     print("")
@@ -101,10 +111,22 @@ commands_functions = {
     6: {"description": "Alterar tamanho de chunk", "function": change_chunk_size},
     EXIT_COMMAND: {"description": "Sair", "function": exit}
 }
+
+def verify_shared_dir(shared_dir: str) -> None:
+    shared_dir = os.path.abspath(shared_dir)  # Resolve to absolute path
+    if not os.path.exists(shared_dir):
+        raise RuntimeError(f"Erro: O diretório compartilhado '{shared_dir}' não existe.")
+    if not os.path.isdir(shared_dir):
+        raise RuntimeError(f"Erro: O caminho '{shared_dir}' não é um diretório.")
+    if not os.access(shared_dir, os.R_OK):
+        raise RuntimeError(f"Erro: O diretório compartilhado '{shared_dir}' não é acessível para leitura.")
+    global _shared_dir 
+    _shared_dir = shared_dir
+
 def main():
     address, port = sys.argv[1].split(':')
     peers_file = sys.argv[2]
-    shared_dir = sys.argv[3]
+    verify_shared_dir(sys.argv[3])
 
     global connection #TODO: Verificar se é uma boa prática e o melhor jeito de fazer isso
     connection = Connection(address, port, peer_manager)
